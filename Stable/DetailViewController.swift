c6827aac4627cf2e24d180643f4fb13410a1b8f5
Saameh Malik <saamehmalik@gmail.com>
2015-10-20 10:04:25 -0700

    Adding files from Pied Piper

--- Stable/DetailViewController.swift
+++ Stable/DetailViewController.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
-//  Stable
+//  DetailViewController.swift
+//  Pied Piper
 //
-//  Created by Saameh Malik on 10/20/15.
+//  Created by Saameh Malik on 10/14/15.
 //  Copyright © 2015 Saameh Malik. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class DetailViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
---
+++ Stable/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  Pied Piper
+//
+//  Created by Saameh Malik on 10/14/15.
+//  Copyright © 2015 Saameh Malik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    let dorms = ["North Mountain", "Red Bricks", "Towers", "PCV"]
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dorms.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return dorms[row]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
